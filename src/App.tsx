184a8391d96a85fe8e92c518a67afc2f4fa8ffaf
António Jacinto <antjacinto11672@gamail.com>
2025-04-02 14:44:04 +0100

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <header>
+          <div><img src=""  alt=""/><p>António Jacinto</p></div>
+          <nav>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">sobre</a></li>
+              <li><a href="">portifólio</a></li>
+              <li><a href="">serviços</a></li>
+              <li><a href="">resumo</a></li>
+              <li><a href="">contato</a></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
