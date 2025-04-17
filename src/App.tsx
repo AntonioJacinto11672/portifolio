fc3c1a3665c02aa6d96b6336e5cbfe77c29acd2d
Sembas <celsiobartolomeu07@gmail.com>
2025-04-17 19:46:40 +0100

    Minhas tech, meu portifolio e experencias profissionais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { FaLinkedinIn } from "react-icons/fa"
-import { FaGithub } from "react-icons/fa6"
+import { FaLaravel, FaLinkedinIn, FaReact, FaSass } from "react-icons/fa"
+import { FaGithub, FaNodeJs } from "react-icons/fa6"
 import { IoLogoInstagram } from "react-icons/io"
 import Title from "./components/Title"
 import Skills from "./components/Skills"
@@ -10,6 +10,11 @@ import { useState } from "react"
 import WebApps from "./components/works/WebApps"
 import MobileApps from "./components/works/MobileApps"
 import OthersApps from "./components/works/OthersApps"
+import { IoLogoCss3, IoLogoHtml5, IoLogoJavascript } from "react-icons/io5"
+import { SiPhp, SiTailwindcss, SiTypescript } from "react-icons/si"
+import { TfiQuoteRight } from "react-icons/tfi"
+import { RiDoubleQuotesR } from "react-icons/ri"
+
 
 const componentProject = [
   <Allapps />,
@@ -144,6 +149,117 @@ function App() {
             </div>
           </div>
         </section>
+
+        <Divide title="Minhas techs" />
+
+        <section className=" bg-gray-100">
+
+          <div className="mx-20 p-4 flex flex-col gap-y-2 items-center my-5 text-gray-600 py-20">
+            <div className="mx-auto">
+              <Title firstTitle="Minhas" secondTitle="Techs" />
+            </div>
+
+            <div className="text-center mt-20">
+              <p className="text-md">Tecnologias</p>
+              <h2 className="capitalize font-bold text-3xl">Já  <span className="text-gray-800">Utilizadas</span></h2>
+            </div>
+            {/* icons */}
+            <div className="flex gap-x-14 justify-between text-gray-600 mt-10">
+              <FaNodeJs size={75} />
+              <SiPhp size={75} className="text-gray-900" />
+              <FaLaravel size={75} />
+              <FaSass size={75} />
+            </div>
+
+
+            <div className="text-center mt-20">
+              <p className="text-md">Tecnologias</p>
+              <h2 className="capitalize font-bold text-3xl">Atualmente   <span className="text-gray-800">Desenvolvendo</span></h2>
+            </div>
+            {/* icons */}
+            <div className="flex gap-x-14 justify-between text-gray-600 mt-10 flex-wrap">
+              <IoLogoHtml5 size={75} className="" />
+              <IoLogoCss3 size={75} className="" />
+              <IoLogoJavascript size={75} className="" />
+              <SiTypescript size={75} />
+              <SiTailwindcss size={75} className="text-gray-900" />
+              <FaReact size={75} />
+            </div>
+          </div>
+        </section>
+
+        <Divide title="Minhas techs" />
+
+        <section className=" bg-gray-100">
+          <div className="mx-20 p-4">
+
+            <div className="my-10 flex flex-row-reverse items-end">
+              <Title firstTitle="Experiência" secondTitle="Profissional" brState={true} />
+              <p className="font-bold  text-md pr-20 text-right">Minha jornada esta apenas <br /> começando, sempre aprendendo</p>
+            </div>
+
+
+            {/* end buttons */}
+            {/* init projects */}
+            {/*  <div className="flex flex-col  items-center my-5 text-gray-600 ">
+              <div className="flex flex-wrap gap-x-6 flex-row items-stretch">
+                <div className="bg-gray-900 w-[300px] h-[190px] flex-grow  self-stretch rounded-2xl overflow-auto cursor-pointer" title="Sistema de gestão de mecanica">
+                  <div className="text-center top-0 left-0 w-full h-full bg-gray-900 opacity-50 rounded-2xl flex flex-col justify-center items-center text-white">
+                    <h4 className="text-white text-2xl">Sistema de gestão de mecânica</h4>
+                    <p className="text-white">Sistema que permite controlar, os funcoionários, trabalhos feitos, entra e saída de uma mecânica... </p>
+                  </div>
+                </div>
+                <div className="bg-gray-600 w-[300px] h-[150px] flex-grow self-stretch rounded-2xl">2</div>
+                <div className="bg-gray-300 w-[300px] h-[190px] flex-grow self-stretch rounded-2xl">3</div>
+                
+              </div>
+            </div> */}
+
+            <div className="flex flex-col items-center justify-center space-y-10 p-10">
+              {/* Cards */}
+              <div className="flex flex-col md:flex-row gap-6 items-center">
+                {/* Card 1 (mais baixo) */}
+                <div className="bg-gray-800 text-white p-6 rounded-lg w-72 text-center shadow-md mt-4 md:mt-16">
+                  <h3 className="text-lg font-bold text-gray-300 mb-2">Cargo</h3>
+                  <p className="text-sm">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                  </p>
+                </div>
+
+                {/* Card 2 (mais elevado) */}
+                <div className="bg-gray-600 text-white p-6 rounded-lg w-72 text-center shadow-md mt-0 md:mt-2">
+                  <h3 className="text-lg font-bold text-gray-300 mb-2">Cargo</h3>
+                  <p className="text-sm">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                  </p>
+                </div>
+
+                {/* Card 3 (mais baixo e com borda azul) */}
+                <div className="bg-gray-800 text-white p-6 rounded-lg w-72 text-center shadow-md border-2 mt-4 md:mt-16">
+                  <h3 className="text-lg font-bold text-gray-300 mb-2">Cargo</h3>
+                  <p className="text-sm">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                  </p>
+                </div>
+              </div>
+
+              {/* Citação */}
+              <div className="text-center max-w-xl mx-auto flex flex-col items-center">
+                <div className="text-3xl text-gray-600 mb-4"> <RiDoubleQuotesR  size={35} /> </div>
+                <p className="text-gray-600 text-lg">
+                  <span className="font-bold text-gray-900 ">“Aprender”</span> é a única coisa que a <span className="font-bold">mente não se cansa</span>,
+                  nunca tem medo e nunca se arrepende
+                </p>
+                <p className="mt-2 font-bold text-gray-900 text-xl">~Leonardo da Vinci</p>
+              </div>
+            </div>
+
+          </div>
+        </section>
+
       </main>
     </>
   )
